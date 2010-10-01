1ab6caaea3ed6d384a7f1ac54c9767b26ebb71a1
Richard Kersey <github@razorit.com>
2010-10-01 15:30:06 -0400

    updated cke helper to support the specification of buttons per editor instance

--- app/views/helpers/cke.php
+++ app/views/helpers/cke.php
@@ -3,7 +3,7 @@ class CkeHelper extends Helper {
 
     var $helpers = Array('Html', 'Javascript'); 
 
-    function load($id, $path) { 
+    function load($id, $path, $buttons) { 
         $did = ''; 
         foreach (explode('.', $id) as $v) { 
             $did .= ucfirst($v); 
@@ -13,10 +13,34 @@ class CkeHelper extends Helper {
   			var field = CKEDITOR.replace( '".$did."' );
   			CKFinder.setupCKEditor(field, '".$path."') ;
         ";*/ 
+		if (isset($buttons)) {
+			$buttonList = $this->__buttonsList($buttons);
+		} else {
+			$buttonList = '';
+		}
+		
         $code = "
-  			var field = CKEDITOR.replace( '".$did."' );
+  			var field = CKEDITOR.replace( '".$did."' ".$buttonList.");
         "; 
         return $this->Javascript->codeBlock($code);  
     } 
+	
+	function __buttonsList($buttons) {
+		if (is_array($buttons)) {
+			$button = ", {
+					toolbar :
+					[
+						[";
+			foreach ($buttons as $but) {
+				$button .= "'".$but."',";
+			}
+			$button .= "]
+					]
+				}";
+			return $button;
+		} else {
+			return false;
+		}
+	}
 } 
 ?>
