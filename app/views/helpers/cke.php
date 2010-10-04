1fe88e4e90b7d335f3486b7c76856c09a69583f1
Richard Kersey <github@razorit.com>
2010-10-03 22:03:34 -0400

    double checked the need for thumbnails on CrimeTV

--- app/views/helpers/cke.php
+++ app/views/helpers/cke.php
@@ -1,9 +1,9 @@
-<?php 
+	<?php 
 class CkeHelper extends Helper { 
 
     var $helpers = Array('Html', 'Javascript'); 
 
-    function load($id, $path, $buttons) { 
+    function load($id, $path, $settings) { 
         $did = ''; 
         foreach (explode('.', $id) as $v) { 
             $did .= ucfirst($v); 
@@ -13,34 +13,63 @@ class CkeHelper extends Helper {
   			var field = CKEDITOR.replace( '".$did."' );
   			CKFinder.setupCKEditor(field, '".$path."') ;
         ";*/ 
-		if (isset($buttons)) {
-			$buttonList = $this->__buttonsList($buttons);
+		if (!empty($settings)) {
+			$configuration = $this->__config($settings);
 		} else {
-			$buttonList = '';
+			$configuration = '';
 		}
 		
         $code = "
-  			var field = CKEDITOR.replace( '".$did."' ".$buttonList.");
+  			var field = CKEDITOR.replace( '".$did."', {".$configuration."});
         "; 
         return $this->Javascript->codeBlock($code);  
     } 
 	
-	function __buttonsList($buttons) {
-		if (is_array($buttons)) {
-			$button = ", {
+	# this should be updated so that it works from an array
+	# like this $config = array('buttons' => array('button1', 'button2'), 'uiColor' => '#000000');
+	function __config($settings) {
+		# color settings
+		if (!empty($settings['uiColor'])) {
+			$color = "uiColor: '".$settings['uiColor']."',";
+		}
+		
+		#button settings
+		if (is_array($settings['buttons'])) {
+			$button = "
 					toolbar :
 					[
 						[";
-			foreach ($buttons as $but) {
+			foreach ($settings['buttons'] as $but) {
 				$button .= "'".$but."',";
 			}
 			$button .= "]
-					]
-				}";
-			return $button;
+					]";
+					
+		}
+		
+		if (!empty($color)) {
+			# add in color if it exsists
+			if (!empty($output)) {
+				$output .= $color;
+			} else {
+				$output = $color;
+			}				
+		}
+		if (!empty($button)) {
+			# add in buttons if they exist
+			if (!empty($output)) {
+				$output .= $button;
+			} else {
+				$output = $button;
+			}
+		}
+		
+		if (!empty($output)) {
+			return $output;
 		} else {
 			return false;
 		}
 	}
+	
 } 
 ?>
