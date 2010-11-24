1a4263d329e17617eb4b84aa630e71799a07bae9
Richard Kersey <github@razorit.com>
2010-11-24 09:45:19 -0500

    Fixed a conflict that was in login helper, and undetected.
    We just copied the file from the dev branch, as we were unaware of the conflict text that was left over in the login.php file of the login helper.

--- app/views/helpers/login.php
+++ app/views/helpers/login.php
@@ -1,35 +1,6 @@
 <?php
 
 class LoginHelper extends AppHelper {
-<<<<<<< HEAD
-    var $beforeValue = '';
-    var $afterValue = '';
-	var $helpers = array('Session', 'Html');
-
-    function afterRender() {
-        $view = ClassRegistry::getObject('view');
-		$user_id = $this->Session->read('Auth.User.id');
-		
-		if (!empty($user_id)) {
-			$login = 'Welcome, <a href="/profiles/profiles/view/user_id:'.$this->Session->read('Auth.User.id').'">'.$this->Session->read('Auth.User.username').'</a> -  <a href="/users/logout">Logout</a>';
-		} else {
-			$login = '<a href="/users/add">Sign Up</a> - <a href="/users/login">Sign In</a> ';
-		}
-			
-		
-    	$view->set('login_for_layout', $login);
-		
-    }
-
-    function setValue($beforeValue = null, $afterValue = null, $renderDefault = true) {
-        $this->beforeValue = $beforeValue;
-        $this->renderDefault = $renderDefault;
-        $this->afterValue = $afterValue;
-    }
-}
-?>
-
-=======
     var $value = '';
 	var $helpers = array('Session', 'Html');
 
@@ -94,4 +65,3 @@ class LoginHelper extends AppHelper {
     }
 }
 ?>
->>>>>>> dev
