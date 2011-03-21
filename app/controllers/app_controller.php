d9e53962b55b19b3384413785fd62372829eddaa
Richard Kersey <richard@razorit.com>
2011-03-21 11:51:47 -0400

    Fixed some inconsequential bugs in maps plugin.

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -116,7 +116,6 @@ class AppController extends Controller {
 		/**
 		 * Implemented for allowing guests access through db acl control
 		 */ #$this->Auth->allow('*');
-		$userId = $this->Auth->user('id');
 		$allowed = array_search($this->params['action'], $this->Auth->allowedActions);
 		if ($allowed === 0 || $allowed > 0 ) {
 			$this->Auth->allow('*');
