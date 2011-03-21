a1f212cfd2d409c80dad72d1ce27a58d4e0f278f
unknown <enbake@.(none)>
2011-03-21 13:31:50 +0530

    registration form with custom forms
    and checkout page has shhiping and billing address auto filled

--- app/config/core.php
+++ app/config/core.php
@@ -48,7 +48,8 @@
  *    Configure::write('log', E_ERROR | E_WARNING);
  *    Configure::write('log', E_ALL ^ E_NOTICE);
  */
-	Configure::write('log', true);
+	Configure::write('log', E_ALL ^ E_NOTICE);
+	//Configure::write('log', true);
 
 /**
  * Application wide charset encoding
