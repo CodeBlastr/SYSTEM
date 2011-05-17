fd2760fbecdd578f2c0a75372ed06e0a3b591eac
Richard Kersey <richard@razorit.com>
2011-05-17 17:53:21 -0400

    First pass of ajax form validation done.
    The forms element now supports ajax validation of the fields you input for email, and number checking, and whether the field is required or not.  Additional validation checks can be added as needed using the form_inputs table.  This update is good because you get instant feedback while you are filling out custom forms for whether you are entering the correct data.
    
    To test create a form, and add a form input that has is_required set to true. With the next commit you can also choose the validation type from a drop down in the admin to add to the is_required or use without the is_required to validate optional fields as well.

--- app/webroot/js/system/system.js
+++ app/webroot/js/system/system.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-$(function() {
+$().ready(function() {
 /* site wide toggle, set the click elements class to toggleClick, and the name attribute to the id of the element you want to toggle */
 	$(".toggleClick").click(function () {
 		var currentName = $(this).attr('name');
