ee6235b5d4f9b19996796ee4edeb8310989759a2
Arvind K <arvind.mailto@gmail.com>
2011-09-22 01:36:39 +0530

    re-uploading changes

--- app/config/database.default.php
+++ app/config/database.default.php
@@ -14,7 +14,7 @@
  * Must retain the above copyright notice and release modifications publicly.
  *
  * @copyright     Copyright 2009-2010, Zuha Foundation Inc. (http://zuha.com)
- * @link          http://zuha.com Zuha™ Project
+ * @link          http://zuha.com Zuhaï¿½ Project
  * @package       zuha
  * @subpackage    zuha.app.config
  * @since         Zuha(tm) v 0.0.1
