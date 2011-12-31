ee83e87380889604783d07751ea1380db4760b22
Richard Kersey <github@razorit.com>
2011-12-31 15:20:47 -0500

    More Gallery Testing in the Zoomable Type.
    Fixed some formatting of the edit page, and double checked that image resizing worked easily.
    
    NOTE : Changed the dynamic images to use the large version of the image for all conversions, so that the highest quality resize would come out.

--- app/View/Helper/HtmlHelper.php
+++ app/View/Helper/HtmlHelper.php
@@ -1241,191 +1241,191 @@ class HtmlHelper extends AppHelper {
  */
     private function _resizeImage($cType = 'resize', $id, $imgFolder, $newName = false, $newWidth=false, $newHeight=false, $quality = 75, $bgcolor = false) {
         $img = $imgFolder . $id;
-        list($oldWidth, $oldHeight, $type) = getimagesize($img); 
-        $ext = $this->image_type_to_extension($type);
+		if (file_exists($img)) {
+	        list($oldWidth, $oldHeight, $type) = getimagesize($img); 
+	        $ext = $this->image_type_to_extension($type);
 		
-		# check for and create cacheFolder
-		$cacheFolder = 'cache';
-		$cachePath = $imgFolder . $cacheFolder;
-		if (is_dir($cachePath)) {
-			# do nothing the cache dir exists
-		} else {
-			if (mkdir($cachePath)) {
+			# check for and create cacheFolder
+			$cacheFolder = 'cache';
+			$cachePath = $imgFolder . $cacheFolder;
+			if (is_dir($cachePath)) {
 				# do nothing the cache dir exists
 			} else {
-				debug('Could not make images ' . $cachePath . ', and it doesn\'t exist.');
-				break;
+				if (mkdir($cachePath)) {
+					# do nothing the cache dir exists
+				} else {
+					debug('Could not make images ' . $cachePath . ', and it doesn\'t exist.');
+					break;
+				}
 			}
+	        
+	        //check to make sure that the file is writeable, if so, create destination image (temp image)
+	        if (is_writeable($cachePath)) {
+	            if($newName){
+	                $dest = $cachePath .DS . $newName. '.' . $id;
+	            } else {
+	                $dest = $cachePath . DS . 'tmp_' . $id;
+	            }
+	        } else {
+	            //if not let developer know
+	            $imgFolder = substr($imgFolder, 0, strlen($imgFolder) -1);
+	            $imgFolder = substr($imgFolder, strrpos($imgFolder, '\\') + 1, 20);
+	            debug("You must allow proper permissions for image processing. And the folder has to be writable.");
+	            debug("Run \"chmod 777 on '$imgFolder' folder\"");
+	            exit();
+	        }
+	        
+	        //check to make sure that something is requested, otherwise there is nothing to resize.
+	        //although, could create option for quality only
+	        if ($newWidth || $newHeight) {
+	            /*
+	             * check to make sure temp file doesn't exist from a mistake or system hang up.
+	             * If so delete.
+	             */
+	            if(file_exists($dest)) {
+					$size = @getimagesize($dest);
+					return array(
+						'path' => $cacheFolder . '/' . $newName. '.' . $id,
+						'width' => $size[0],
+						'height' => $size[1],
+						);				
+	                #unlink($dest);
+	            } else {
+	                switch ($cType){
+	                    default:
+	                    case 'resize':
+	                        # Maintains the aspect ration of the image and makes sure that it fits
+	                        # within the maxW(newWidth) and maxH(newHeight) (thus some side will be smaller)
+	                        $widthScale = 2;
+	                        $heightScale = 2;
+	                        
+	                        if($newWidth) $widthScale = $newWidth / $oldWidth;
+	                        if($newHeight) $heightScale = $newHeight / $oldHeight;
+	                        //debug("W: $widthScale  H: $heightScale<br>");
+	                        if($widthScale < $heightScale) {
+	                            $maxWidth = $newWidth;
+	                            $maxHeight = false;                            
+	                        } elseif ($widthScale > $heightScale ) {
+	                            $maxHeight = $newHeight;
+    	                        $maxWidth = false;
+	                        } else {
+	                            $maxHeight = $newHeight;
+	                            $maxWidth = $newWidth;
+	                        }
+	                        
+	                        if($maxWidth > $maxHeight){
+	                            $applyWidth = $maxWidth;
+	                            $applyHeight = ($oldHeight*$applyWidth)/$oldWidth;
+	                        } elseif ($maxHeight > $maxWidth) {
+	                            $applyHeight = $maxHeight;
+	                            $applyWidth = ($applyHeight*$oldWidth)/$oldHeight;
+	                        } else {
+	                            $applyWidth = $maxWidth; 
+	                            $applyHeight = $maxHeight;
+	                        }
+	                        #debug("oW: $oldWidth oH: $oldHeight mW: $maxWidth mH: $maxHeight<br>");
+	                       	#debug("aW: $applyWidth aH: $applyHeight<br>");
+	                        $startX = 0;
+	                        $startY = 0;
+	                        #exit();
+	                        break;
+	                    case 'resizeCrop':
+	                        // -- resize to max, then crop to center
+	                        $ratioX = $newWidth / $oldWidth;
+	                        $ratioY = $newHeight / $oldHeight;
+	    
+	                        if ($ratioX < $ratioY) { 
+	                            $startX = round(($oldWidth - ($newWidth / $ratioY))/2);
+	                            $startY = 0;
+	                            $oldWidth = round($newWidth / $ratioY);
+	                            $oldHeight = $oldHeight;
+	                        } else { 
+	                            $startX = 0;
+	                            $startY = round(($oldHeight - ($newHeight / $ratioX))/2);
+	                            $oldWidth = $oldWidth;
+	                            $oldHeight = round($newHeight / $ratioX);
+	                        }
+	                        $applyWidth = $newWidth;
+	                        $applyHeight = $newHeight;
+	                        break;
+	                    case 'crop':
+	                        // -- a straight centered crop
+	                        $startY = ($oldHeight - $newHeight)/2;
+	                        $startX = ($oldWidth - $newWidth)/2;
+	                        $oldHeight = $newHeight;
+	                        $applyHeight = $newHeight;
+	                        $oldWidth = $newWidth; 
+	                        $applyWidth = $newWidth;
+	                        break;
+	                }
+	                
+	                switch($ext) {
+	                    case 'gif' :
+	                        $oldImage = imagecreatefromgif($img);
+	                        break;
+	                    case 'png' :
+	                        $oldImage = imagecreatefrompng($img);
+	                        break;
+	                    case 'jpg' :
+	                    case 'jpeg' :
+	                        $oldImage = imagecreatefromjpeg($img);
+	                        break;
+	                    default :
+	                        //image type is not a possible option
+	                        return false;
+	                        break;
+	                }
+	                
+	                //create new image
+	                $newImage = imagecreatetruecolor($applyWidth, $applyHeight);
+	                
+	                if($bgcolor) {
+	                //set up background color for new image
+	                    sscanf($bgcolor, "%2x%2x%2x", $red, $green, $blue);
+	                    $newColor = ImageColorAllocate($newImage, $red, $green, $blue); 
+	                    imagefill($newImage,0,0,$newColor);
+					};
+	                
+	                //put old image on top of new image
+	                imagecopyresampled($newImage, $oldImage, 0,0 , $startX, $startY, $applyWidth, $applyHeight, $oldWidth, $oldHeight);	                
+    	                switch($ext) {
+	                        case 'gif' :
+	                            imagegif($newImage, $dest, $quality);
+	                            break;
+    	                    case 'png' :
+	                            imagepng($newImage, $dest, $quality);
+	                            break;
+	                        case 'jpg' :
+	                        case 'jpeg' :
+	                            imagejpeg($newImage, $dest, $quality);
+	                            break;
+	                        default :
+	                            return false;
+	                            break;
+	                    }
+                
+         	       imagedestroy($newImage);
+	                imagedestroy($oldImage);
+	                
+	                if(!$newName){
+	                    unlink($img);
+	                    rename($dest, $img);
+	                }
+	                
+					$size = @getimagesize($cacheFolder . '/' . $newName. '.' . $id);
+					return array(
+						'path' => $cacheFolder . '/' . $newName. '.' . $id,
+						'width' => $applyWidth,
+						'height' => $applyHeight,
+						);	
+	            }
+	
+	        } else {
+	            return false;
+	        }
+		} else {
+			return false; // end the check for if the file to convert even exists
 		}
-        
-        //check to make sure that the file is writeable, if so, create destination image (temp image)
-        if (is_writeable($cachePath)) {
-            if($newName){
-                $dest = $cachePath .DS . $newName. '.' . $id;
-            } else {
-                $dest = $cachePath . DS . 'tmp_' . $id;
-            }
-        } else {
-            //if not let developer know
-            $imgFolder = substr($imgFolder, 0, strlen($imgFolder) -1);
-            $imgFolder = substr($imgFolder, strrpos($imgFolder, '\\') + 1, 20);
-            debug("You must allow proper permissions for image processing. And the folder has to be writable.");
-            debug("Run \"chmod 777 on '$imgFolder' folder\"");
-            exit();
-        }
-        
-        //check to make sure that something is requested, otherwise there is nothing to resize.
-        //although, could create option for quality only
-        if ($newWidth || $newHeight) {
-            /*
-             * check to make sure temp file doesn't exist from a mistake or system hang up.
-             * If so delete.
-             */
-            if(file_exists($dest)) {
-				$size = @getimagesize($dest);
-				return array(
-					'path' => $cacheFolder . '/' . $newName. '.' . $id,
-					'width' => $size[0],
-					'height' => $size[1],
-					);				
-                #unlink($dest);
-            } else {
-                switch ($cType){
-                    default:
-                    case 'resize':
-                        # Maintains the aspect ration of the image and makes sure that it fits
-                        # within the maxW(newWidth) and maxH(newHeight) (thus some side will be smaller)
-                        $widthScale = 2;
-                        $heightScale = 2;
-                        
-                        if($newWidth) $widthScale = $newWidth / $oldWidth;
-                        if($newHeight) $heightScale = $newHeight / $oldHeight;
-                        //debug("W: $widthScale  H: $heightScale<br>");
-                        if($widthScale < $heightScale) {
-                            $maxWidth = $newWidth;
-                            $maxHeight = false;                            
-                        } elseif ($widthScale > $heightScale ) {
-                            $maxHeight = $newHeight;
-                            $maxWidth = false;
-                        } else {
-                            $maxHeight = $newHeight;
-                            $maxWidth = $newWidth;
-                        }
-                        
-                        if($maxWidth > $maxHeight){
-                            $applyWidth = $maxWidth;
-                            $applyHeight = ($oldHeight*$applyWidth)/$oldWidth;
-                        } elseif ($maxHeight > $maxWidth) {
-                            $applyHeight = $maxHeight;
-                            $applyWidth = ($applyHeight*$oldWidth)/$oldHeight;
-                        } else {
-                            $applyWidth = $maxWidth; 
-                            $applyHeight = $maxHeight;
-                        }
-                        #debug("oW: $oldWidth oH: $oldHeight mW: $maxWidth mH: $maxHeight<br>");
-                       	#debug("aW: $applyWidth aH: $applyHeight<br>");
-                        $startX = 0;
-                        $startY = 0;
-                        #exit();
-                        break;
-                    case 'resizeCrop':
-                        // -- resize to max, then crop to center
-                        $ratioX = $newWidth / $oldWidth;
-                        $ratioY = $newHeight / $oldHeight;
-    
-                        if ($ratioX < $ratioY) { 
-                            $startX = round(($oldWidth - ($newWidth / $ratioY))/2);
-                            $startY = 0;
-                            $oldWidth = round($newWidth / $ratioY);
-                            $oldHeight = $oldHeight;
-                        } else { 
-                            $startX = 0;
-                            $startY = round(($oldHeight - ($newHeight / $ratioX))/2);
-                            $oldWidth = $oldWidth;
-                            $oldHeight = round($newHeight / $ratioX);
-                        }
-                        $applyWidth = $newWidth;
-                        $applyHeight = $newHeight;
-                        break;
-                    case 'crop':
-                        // -- a straight centered crop
-                        $startY = ($oldHeight - $newHeight)/2;
-                        $startX = ($oldWidth - $newWidth)/2;
-                        $oldHeight = $newHeight;
-                        $applyHeight = $newHeight;
-                        $oldWidth = $newWidth; 
-                        $applyWidth = $newWidth;
-                        break;
-                }
-                
-                switch($ext) {
-                    case 'gif' :
-                        $oldImage = imagecreatefromgif($img);
-                        break;
-                    case 'png' :
-                        $oldImage = imagecreatefrompng($img);
-                        break;
-                    case 'jpg' :
-                    case 'jpeg' :
-                        $oldImage = imagecreatefromjpeg($img);
-                        break;
-                    default :
-                        //image type is not a possible option
-                        return false;
-                        break;
-                }
-                
-                //create new image
-                $newImage = imagecreatetruecolor($applyWidth, $applyHeight);
-                
-                if($bgcolor):
-                //set up background color for new image
-                    sscanf($bgcolor, "%2x%2x%2x", $red, $green, $blue);
-                    $newColor = ImageColorAllocate($newImage, $red, $green, $blue); 
-                    imagefill($newImage,0,0,$newColor);
-                endif;
-                
-                //put old image on top of new image
-                imagecopyresampled($newImage, $oldImage, 0,0 , $startX, $startY, $applyWidth, $applyHeight, $oldWidth, $oldHeight);
-                
-                    switch($ext)
-                    {
-                        case 'gif' :
-                            imagegif($newImage, $dest, $quality);
-                            break;
-                        case 'png' :
-                            imagepng($newImage, $dest, $quality);
-                            break;
-                        case 'jpg' :
-                        case 'jpeg' :
-                            imagejpeg($newImage, $dest, $quality);
-                            break;
-                        default :
-                            return false;
-                            break;
-                    }
-                
-                imagedestroy($newImage);
-                imagedestroy($oldImage);
-                
-                if(!$newName){
-                    unlink($img);
-                    rename($dest, $img);
-                }
-                
-				$size = @getimagesize($cacheFolder . '/' . $newName. '.' . $id);
-				return array(
-					'path' => $cacheFolder . '/' . $newName. '.' . $id,
-					'width' => $applyWidth,
-					'height' => $applyHeight,
-					);	
-            }
-
-        } else {
-            return false;
-        }
-        
-
     }
 
     function image_type_to_extension($imagetype) {
