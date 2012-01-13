d7aa52adba93897e5174c77b26701fd88fb62924
Richard Kersey <github@razorit.com>
2012-01-12 21:15:55 -0500

    Deleted Array Source and Template.
    I think they were there not as code for zuha, but as some kind of files needed for some ide program, that I don't know of anyone using except an old russian programmer.

---
+++ app/Test/Fixture/SettingFixture.php
@@ -0,0 +1,17 @@
+<?php
+/* Setting Fixture generated on: 2012-01-13 02:10:14 : 1326420614 */
+
+/**
+ * SettingFixture
+ *
+ */
+class SettingFixture extends CakeTestFixture {
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('model' => 'Setting', 'records' => true, 'connection' => 'test');
+
+
+}
