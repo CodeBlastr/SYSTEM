96c05f883a0554e68a07a216088849c12292d8fe
Richard Kersey <github@razorit.com>
2012-04-16 14:55:55 -0400

    trying to upgrade to Cake 2.0

--- app/index.php
+++ app/index.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * PHP version 5.3 
+ * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://zuha.com Zuha™ Project
- * @package       cake
- * @subpackage    cake.app
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
