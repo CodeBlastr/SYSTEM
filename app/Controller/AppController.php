e295050b22742b75a4fd2870ed37c49be24f3474
Richard Kersey <github@razorit.com>
2012-04-19 09:56:01 -0400

    More simple json views

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -132,8 +132,9 @@ class AppController extends Controller {
 	
 	
     public function beforeRender() {
+		parent::beforeRender();
 		$this->set('referer', 'test'); // used for back button links, could be useful for breadcrumbs possibly
-		$this->jsonSerialize = true; // used in View/JsonView to auto serialize
+		$this->set('_serialize', array_keys($this->viewVars));
 	}
 
 
