897080fe39a5e45e5a9cda16aee34edf6bb3b670
Joel Byrnes <joel@razorit.com>
2012-05-01 18:33:13 -0400

    probably just whitespace from me debugging

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -23,7 +23,7 @@
 
 App::uses('Model', 'Model');
 
-class AppModel extends Model {  
+class AppModel extends Model {
 
 	public $actsAs = array('Containable');
   	public $recursive = -1;
@@ -39,7 +39,7 @@ class AppModel extends Model {
 	    	# There may be a potential problem with this.
 			# It saves an ArosAco record for every record being created.
 	      	# For example, when creating a webpage, it also creates an Aco for the Alias
-	      	# Left it in as is, because we may want this.  (ie. when a contact is record level, 
+	      	# Left it in as is, because we may want this.  (ie. when a contact is record level,
 	      	# we probably want the user to have access to the Contact and Contact Person
 	      	# if a project issue is created, we probably want the user to have access to the project too.
       		$this->Behaviors->attach('Acl', array('type' => 'controlled'));
@@ -49,9 +49,9 @@ class AppModel extends Model {
         		$this->Behaviors->attach('Acl', array('type' => 'controlled'));
         		$this->Behaviors->attach('AclExtra', $this->data);
       		}
-    	}   
-    
-    	# Start Auto Creator & Modifier Id Saving # 
+    	}
+
+    	# Start Auto Creator & Modifier Id Saving #
     	$exists = $this->exists();
     	$user = class_exists('CakeSession') ? CakeSession::read('Auth.User') : null;
     	if ( !$exists && $this->hasField('creator_id') && empty($this->data[$this->alias]['creator_id']) ) {
@@ -60,12 +60,12 @@ class AppModel extends Model {
     	if ( $this->hasField('modifier_id') && empty($this->data[$this->alias]['modifier_id']) ) {
       		$this->data[$this->alias]['modifier_id'] = $user['id'];
     	}
-    	# End Auto Creator & Modifier Id Saving # 
+    	# End Auto Creator & Modifier Id Saving #
     	# you have to return true to make the save continue.
     	return true;
   	}
-    
-  
+
+
 /**
  * Condition Check, checks to see if any conditions from the conditions table were met.
  */
@@ -73,18 +73,18 @@ class AppModel extends Model {
 	    # Start Condition Check #
     	$this->Condition = ClassRegistry::init('Condition');
 	    #get the id that was just inserted so you can call back on it.
-	    $this->data[$this->name]['id'] = $this->id;  
-	    
+	    $this->data[$this->name]['id'] = $this->id;
+
 	    if ($created === true) {
 	    	$this->Condition->checkAndFire('is_create', array('model' => $this->name), $this->data);
 	    } else {
 	    	$this->Condition->checkAndFire('is_update', array('model' => $this->name), $this->data);
-			#$this->conditionCheck('is_read'); // this needs to be put into the beforeFilter or beforeRender (beforeRender, would allow error pages to work too) of the 
+			#$this->conditionCheck('is_read'); // this needs to be put into the beforeFilter or beforeRender (beforeRender, would allow error pages to work too) of the
 	    }
     	# End Condition Check #
 	}
-  
-  
+
+
 /**
  * Condition Check, checks to see if any conditions from the conditions table were met.
  */
@@ -93,17 +93,17 @@ class AppModel extends Model {
 	    App::Import('Model', 'Condition');
 	    $this->Condition = new Condition;
 	    #get the id that was just inserted so you can call back on it.
-	    $this->data[$this->name]['id'] = $this->id;  
-	    $this->Condition->checkAndFire('is_delete', array('model' => $this->name), $this->data); 
+	    $this->data[$this->name]['id'] = $this->id;
+	    $this->Condition->checkAndFire('is_delete', array('model' => $this->name), $this->data);
 	    # End Condition Check #
 	}
-  
-  
-  
+
+
+
 /**
  * With this function our total_count now appears with the rest of the fields in the resulting data array.
  * http://nuts-and-bolts-of-cakephp.com/2008/09/29/dealing-with-calculated-fields-in-cakephps-find/
- */ 
+ */
 	public function afterFind($results, $primary = false) {
     	if($primary == true) {
         	if(Set::check($results, '0.0')) {
@@ -113,32 +113,32 @@ class AppModel extends Model {
 	                unset($results[$key][0]);
 	             }
 			}
-		}    
+		}
 		return $results;
 	}
-  
-  
+
+
 	public function listPlugins($remove = array(), $merge = true) {
 	    $defaultRemove = array('Acl Extras', 'Api Generator', 'Recaptcha', 'Favorites.needs.upgrade', 'Forum.needs.upgrade');
 	    $remove = !empty($merge) ? array_merge($defaultRemove, $remove) : $remove;
 	    $plugins = CakePlugin::loaded();
-	    foreach ($plugins as $plugin) : 
+	    foreach ($plugins as $plugin) :
 			$return[$plugin] = Inflector::humanize(Inflector::underscore($plugin));
 	    endforeach;
-	    
+
 	    return array_diff($return, $remove);
 	}
-  
-  
+
+
 	public function listModels($pluginPath = null, $remove = array(), $merge = true) {
-	    # defaultRemove originally done for this page : /admin/categories/categories/add/ 
+	    # defaultRemove originally done for this page : /admin/categories/categories/add/
 	    # if you add items for removal from this list make sure that they should also be removed from there
 	    # or customize the categories_controller so that listModels() function to not merge
 	    $defaultRemove = array('Affiliated', 'Affiliates App Model', 'Alias', 'App Model', 'Authorize', 'Blogs App Model', 'Catalog Items Catalog Category', 'Catalogs App Model', 'Categories App Model', 'Categorized Option', 'Category', 'Categorized', 'Category Option', 'Catalog Item Price', 'Comments App Model', 'Condition', 'Contacts App Model', 'Contacts Contact', 'Coupons App Model', 'Credits App Model', 'Enumeration', 'Estimates App Model', 'Events App Model', 'Estimated', 'Form', 'Form Fieldset', 'Form Input', 'Forms App Model', 'Forum', 'Forum Category', 'Galleries App Model', 'Poll', 'Poll Option', 'Poll Vote', 'Post', 'Setting', 'Topic', 'Invite', 'Invite App Model', 'Invoices Catalog Item', 'Invoices App Model', 'Invoices Timesheet', 'Maps App Model', 'Media App Model', 'Members App Model', 'Menus App Model', 'Messages App Model', 'News App Model', 'Notifications App Model', 'Notification', 'Notification Template', 'Orders App Model', 'Favorite', 'Privilege', 'Privileges App Model', 'Project Issue', 'Projects.watcher', 'Projects App Model', 'Projects Member', 'Ratings App Model', 'Records App Model', 'Reports App Model', 'Requestor', 'Section', 'Search Index', 'Searchable App Model', 'Sitemaps App Model', 'Tags App Model', 'Tasks App Model', 'Tickets App Model', 'Ticket Departments Assignee', 'Timesheets Timesheet Time', 'Timesheets App Model', 'Used', 'User Role', 'Users App Model', 'Users User Group', 'Utils App Model', 'Webpages App Model', 'Wiki Content Version', 'Wikis App Model', 'Workflows App Model', 'Workflow', 'Workflow Item', 'Workflow Event', 'Workflow Item Event', 'Zencoder');
     	$remove = !empty($merge) ? array_merge($defaultRemove, $remove) : $remove;
-	    
+
 	    $plugins = $this->listPlugins();
-	    
+
 	    foreach ($plugins as $plugin) :
 			$models = !empty($models) ? array_merge($models, App::objects($plugin . '.Model')) : App::objects($pluginPath . '.Model');
 	    endforeach;
@@ -147,11 +147,11 @@ class AppModel extends Model {
 	    foreach ($models as $model) :
 			$return[$model] = Inflector::humanize(Inflector::underscore($model));
 	    endforeach;
-    
+
     	return array_diff($return, $remove);
 	}
-	
-	
+
+
 /**
  * Don't know what this is for, I'd like to see a comment placed. OLD
  * Update... parentNode has something to do with ACL and how they are saved using the Behavior.  Not sure exactly how this one is used though.  1/2/2012 RK
@@ -159,8 +159,8 @@ class AppModel extends Model {
 	public function parentNode() {
 		$this->name;
 	}
-  
-  
+
+
 /**
  * Used to see whether the record being saved is a record which is subject to record level access control.  Executed in the beforeSave callback function of app_model.  If it is a record which is subject to record level access control, then beforeSave triggers the record level Aco and ArosAco creation.  Using the Acl behavior which make the Aco, and the AclExtra behavior which makes the AroAco using the user field which is supposed to get access as the Aro.  To set the Aros that should have access, make a setting called RECORD_LEVEL_ACCESS_ENTITY in the "settings" table of the database.
  *
@@ -184,9 +184,9 @@ class AppModel extends Model {
 	        }
 	      }
 	    }
-    
+
 	    if (!empty($userIds)) {
-	      # 
+	      #
 	      return array('User' => $userIds);
 	    } else {
 	      return false;
@@ -203,7 +203,7 @@ class AppModel extends Model {
         // creates a 6 digit key
         $uid = substr(md5(uniqid(rand(), true)), 0, 40);
         if ($prefix)
-          $uid = $prefix . $uid; 
+          $uid = $prefix . $uid;
         //checkto make sure its not a duplcate
         if ($table) {
         	foreach($table as $model => $col) {
@@ -219,29 +219,29 @@ class AppModel extends Model {
 			return $uid;
 	    }
     }
-	
-	
+
+
     public function _generateUUID() {
         $uuid = $this->query('SELECT UUID() AS uuid');
         return $uuid[0][0]['uuid'];
     }
-	
+
 /**
  * Make sending email available to models (as well as controllers)
- * 
+ *
  * @param string		String - address to send email to
  * @param sring			$subject: subject of email.
- * @param string		$message['html'] in the layout will be replaced with this text. 
+ * @param string		$message['html'] in the layout will be replaced with this text.
  * @param string		$template to be picked from folder for email. By default, if $mail is given in any template.
  * @param array			address/name pairs (e.g.: array(example@address.com => name, ...)
  * @param UNKNOWN		Have not used it don't know what it does or if it works.
- * @return bool			
+ * @return bool
  */
 	public function __sendMail($toEmail = null, $subject = null, $message = null, $template = 'default', $from = array(), $attachment = null) {
 		App::uses('AppController', 'Controller');
 		$Controller = new AppController;
 		return $Controller->__sendMail($toEmail, $subject, $message, $template, $from, $attachment);
 	}
-	
+
 }
 ?>
--- app/Model/Condition.php
+++ app/Model/Condition.php
@@ -2,7 +2,7 @@
 /**
  * Condition Model
  *
- * This model handles the data for conditions.  Conditions are met when the model and sub conditions are a match.  When the conditions are met, the bind_model field will trigger an event in the dynamically binded model called "triggered".  Using the triggered method in a model, will allow you to create any number of actions from a single action which meets the condition. 
+ * This model handles the data for conditions.  Conditions are met when the model and sub conditions are a match.  When the conditions are met, the bind_model field will trigger an event in the dynamically binded model called "triggered".  Using the triggered method in a model, will allow you to create any number of actions from a single action which meets the condition.
  *
  * PHP versions 5
  *
@@ -13,7 +13,7 @@
  * Must retain the above copyright notice and release modifications publicly.
  *
  * @copyright     Copyright 2009-2010, Zuha Foundation Inc. (http://zuha.com)
- * @link          http://zuha.com Zuha™ Project
+ * @link          http://zuha.com Zuhaï¿½ Project
  * @package       zuha
  * @subpackage    zuha.app.models
  * @since         Zuha(tm) v 0.0.1
@@ -28,7 +28,7 @@ class Condition extends AppModel {
 		'creator_id' => array('numeric'),
 		'modifier_id' => array('numeric')
 	);
-	
+
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	public $belongsTo = array(
 		'Creator' => array(
@@ -46,9 +46,9 @@ class Condition extends AppModel {
 			'order' => ''
 		)
 	);
-	
-	
-/** 
+
+
+/**
  * Check the conditions table to see if the record being created, read, updated, or deleted matches any conditions. And if it is then will do a sub condition check, and finally fire the bindModel if condtions are met.
  *
  * @param {type} 		Valid values are, "is_create", "is_read", "is_update", "is_delete".
@@ -78,9 +78,9 @@ class Condition extends AppModel {
 			}
 		}
 	}
-	
-	
-/** 
+
+
+/**
  * This function does a find to see if the any conditions exist which match the current create, read, update or delete action.
  *
  * @param {type} 		Four valid values, 'is_create', 'is_read', 'is_update', 'is_delete'.
@@ -90,7 +90,7 @@ class Condition extends AppModel {
 Test case: ModelDeleteTest(testDeleteHabtmReferenceWithConditions)  FROM All Model Unit Test
  * @todo				SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1
 Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
- */	
+ */
 	public function checkConditions($type, $lookups) {
 		$fields = $this->_conditionConditions($lookups);
 		$conditions = $this->find('all', array(
@@ -106,26 +106,26 @@ Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
 			));
 		return $conditions;
 	}
-	
-	
-	
-/** 
+
+
+
+/**
  * This does a double check on the conditions if the first conditions were matched.  For example, you may save a project, and if the assignee_id is 3, have a different condition OR no condition met.  This function allows sub conditions to exist.
  *
  * @param {condition} 	A string of conditions in this format : Model.field.operator.value,Model.field.operator.value (An example : ContactPerson.email.!=.null,ContactPerson.contact_id.>.0)
  * @param {data}		This is all of the form submitted data, to read and see if the sub condition was met.
  * @return {bool}		Returns true if all sub conditions were met or don't exist, and false if they were not met.
- */	
+ */
 	protected function _checkSubConditions($condition, $data) {
 		if (!empty($condition['Condition']['condition'])) {
-			# check the sub condition code goes here. 
+			# check the sub condition code goes here.
 			$conditionsArray = explode(',',$condition['Condition']['condition']);
 			foreach ($conditionsArray as $conditionsArr) {
 				$conditions[] = explode('.',$conditionsArr);
 			}
-			
+
 			foreach ($conditions as $condition) {
-				# check for the operator 
+				# check for the operator
 				if ($condition[3] == 'null' && $condition[2] == '=') {
 					if (empty($data[$condition[0]][$condition[1]])) {
 						$return = true;
@@ -139,49 +139,49 @@ Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
 					}  else {
 						$return = false;
 						break;
-					}	
+					}
 				} else if ($condition[2] == '=') {
 					if ($data[$condition[0]][$condition[1]] == $condition[3]) {
 						$return = true;
 					} else {
 						$return = false;
 						break;
-					} 	
+					}
 				} else if ($condition[2] == '!=') {
 					if ($data[$condition[0]][$condition[1]] != $condition[3]) {
 						$return = true;
 					} else {
 						$return = false;
 						break;
-					}			
+					}
 				} else if ($condition[2] == '<=') {
 					if ($data[$condition[0]][$condition[1]] <= $condition[3]) {
 						$return = true;
 					} else {
 						$return = false;
 						break;
-					}			
+					}
 				} else if ($condition[2] == '>=') {
 					if ($data[$condition[0]][$condition[1]] >= $condition[3]) {
 						$return =  true;
 					} else {
 						$return = false;
 						break;
-					}	
+					}
 				} else if ($condition[2] == '<') {
 					if ($data[$condition[0]][$condition[1]] < $condition[3]) {
 						$return = true;
 					} else {
 						$return = false;
 						break;
-					}	
+					}
 				} else if ($condition[2] == '>') {
 					if ($data[$condition[0]][$condition[1]] > $condition[3]) {
 						$return = true;
 					} else {
 						$return = false;
 						break;
-					}			
+					}
 				} else {
 					$return = false;
 					break;
@@ -192,28 +192,28 @@ Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
 		}
 		return $return;
 	}
-	
+
 		# This has been saved so that we can use it when we finish of the extra condition checking in the condition model
 	# If it exists there, then delete this function, but NOT until then.
 	protected function __checkExtraCondition($conditionTrigger) {
 	}
-	
-	
-	
-	
-/** 
+
+
+
+
+/**
  * There are four possible condition types.  Create, Read, Update, Delete (CRUD).  If it is the read type then we would use the controller information that is currently being viewed to trigger the event.  Otherwise, we would use the model that is being effected during the create, update, or delete event.  This function returns the conditions necessary to match against.
  *
  * @param {lookups} 	An array with possible indexes of model, plugin, controller action, or extra_values.
  * @return {array}		Returns an array of conditions.
- */	
+ */
 	protected function _conditionConditions($lookups) {
 		$model = !empty($lookups['model']) ? $lookups['model'] : null;
 		$plugin = !empty($lookups['plugin']) ? $lookups['plugin'] : null;
 		$controller = !empty($lookups['controller']) ? $lookups['controller'] : null;
 		$action = !empty($lookups['action']) ? $lookups['action'] : null;
 		$extraValues = !empty($lookups['extra_values']) ? $lookups['extra_values'] : null;
-		
+
 		if (!empty($model)) {
 			$condition = array('Condition.model' => $model);
 		} else {
@@ -226,21 +226,21 @@ Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
 		}
 		return $condition;
 	}
-	
-	
-/** 
- * The final step in a condition being matched.  It calls the model using the bind_model field, and fires an action called "triggered".  This is where the initial event is transferred to an outside plugin or model.  If the triggered event is going to be database driven or a future event, then model you are binding should belongTo Condition, and have a "condition_id" in the table to properly match the action taking place to the condition triggering it.  Otherwise it is possible to just have a triggered action which does all of the work direction from the triggered method in that model. 
+
+
+/**
+ * The final step in a condition being matched.  It calls the model using the bind_model field, and fires an action called "triggered".  This is where the initial event is transferred to an outside plugin or model.  If the triggered event is going to be database driven or a future event, then model you are binding should belongTo Condition, and have a "condition_id" in the table to properly match the action taking place to the condition triggering it.  Otherwise it is possible to just have a triggered action which does all of the work direction from the triggered method in that model.
  *
  * @param {id} 			The id of the condition that was met.
  * @param {model}		The model which belongsTo condition. If a plugin, this should be in Plugin.Model format.
- * @param {data}		An array of data that was originally entered into the form. Use it for creating new actions from the single action that triggered this. 
+ * @param {data}		An array of data that was originally entered into the form. Use it for creating new actions from the single action that triggered this.
  * @return {}			Does not return anything. This is a silent operation, and gives no feedback unless a fatal error occurs.
  */
 	public function fireAction($id, $model, $data) {
 		# import the bind model and fire a model action called triggered.
 		App::import('Model', $model);
 		$thisModel = explode('.', $model);
-		
+
 		if (!empty($thisModel[1])) {
 			# this supports a plugin
 			$this->$thisModel[1] = new $thisModel[1];
@@ -251,8 +251,8 @@ Test case: ModelDeleteTest(testDeleteArticleBLinks) FROM All Model Unit Test
 			$this->$thisModel[0]->triggered($id, $data);
 		}
 	}
-	
-	
+
+
 	public function addRecursiveData($model, $data) {
 		$Model = ClassRegistry::init(ZuhaInflector::pluginize($model) . '.' . $model);
 		$Model->recursive = 1;
