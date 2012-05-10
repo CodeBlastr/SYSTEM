2749a40c35f011e76e0cc51c9a621c8e3e12a233
Richard Kersey <github@razorit.com>
2012-05-10 00:41:18 -0400

    Core needs an else fix

--- app/Config/core.php
+++ app/Config/core.php
@@ -1,4 +1,16 @@
 <?php
 if (defined('SITE_DIR') && file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php')) {
 	require_once(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php');
-} 
+} else {
+	Configure::write('Error', array(
+		'handler' => 'ErrorHandler::handleError',
+		'level' => E_ALL & ~E_DEPRECATED,
+		'trace' => true
+	));
+	
+	Configure::write('Exception', array(
+		'handler' => 'ErrorHandler::handleException',
+		'renderer' => 'AppExceptionRenderer',
+		'log' => true
+	));
+}
