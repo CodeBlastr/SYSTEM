9f813d954edea1282a4c5311cc6a886e1818065f
Richard Kersey <richard@razorit.com>
2012-07-06 10:07:23 -0400

    updating mobile look

---
+++ app/Test/Fixture/ConditionFixture.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * ConditionFixture
+ *
+ */
+class ConditionFixture extends CakeTestFixture {
+
+/**
+ * name property
+ *
+ * @var string
+ */
+	public $name = 'Condition';
+	
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('config' => 'Conditions');
+	
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => '189813-12831-12831923-12312',
+			'name' => 'Instant Coffee',
+			'catalog_item_id' => '189813-12831-12831923-12312',
+			'order_payment_id' => '189813-12831-12831923-12312',
+			'order_shipment_id' => '189813-12831-12831923-12312',
+			'order_transaction_id' => '189813-12831-12831923-12312',
+			'quantity' => 1,
+			'price' => 1,
+			'weight' => 1,
+			'height' => 1,
+			'width' => 1,
+			'length' => 1,
+			'status' => 'incart',
+			'tracking_no' => null,
+			'arb_settings' => null,
+			'payment_type' => null,
+			'featured' => 1,
+			'foreign_key' => '189813-12831-12831923-12312',
+			'model' => 'CatalogItem',
+			'is_virtual' => 1,
+			'customer_id' => 1,
+			'contact_id' => 1,
+			'assignee_id' => 1,
+			'creator_id' => 1,
+			'modifier_id' => 1,
+			'created' => '2012-04-19 17:33:37',
+			'modified' => '2012-04-19 17:33:37'
+		),
+	);
+}
