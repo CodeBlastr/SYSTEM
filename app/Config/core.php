d845b433500f2b103db04d5279d21f0a270b1e00
Richard Kersey <github@razorit.com>
2012-09-10 14:52:08 -0700

    Checked through desktop timer problems

--- app/Config/core.php
+++ app/Config/core.php
@@ -7,14 +7,14 @@ if (defined('SITE_DIR') && file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php
 // THESE ARE ONLY FOR THIS ZUHA SERVER : DO NOT MAKE PUBLIC
 // IT IS WHAT MAKES ZUHA A SAAS 
 
-else if (
+/*else if (
 	!defined('SITE_DIR') && 
 	strpos($_SERVER['HTTP_HOST'], '.zuha.com') &&
 	basename($_SERVER['REQUEST_URI']) == 'site') {
 		define('IS_ZUHA', true);
 		define('SITE_DIR', 'sites'.DS.'zuha.com');
 		require_once(ROOT.DS.'sites'.DS.'zuha.com'.DS.'Config'.DS.'core.php');
-} else if (
+}*/ else if (
 	!defined('SITE_DIR') && 
 	strpos($_SERVER['HTTP_HOST'], '.zuha.com')) {
 		//header('Location: /install/site');
