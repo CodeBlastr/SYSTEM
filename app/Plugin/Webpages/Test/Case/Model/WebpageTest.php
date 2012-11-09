c63b5f56af650006a46f229aa8f0ac15f7249491
Richard Kersey <richard@razorit.com>
2012-11-09 14:22:08 -0500

    Fixed an Alias Bug
    When you edit a webpage it would create multiple alias records unecessarily. This in turn would cause multiple examples of the same webpage showing in the webpages index.
    
    This has been fixed.

---
+++ app/Plugin/Webpages/Test/Case/Model/WebpageTest.php
@@ -0,0 +1,78 @@
+<?php
+App::uses('Webpage', 'Webpages.Model');
+
+/**
+ * Product Test Case
+ *
+ */
+class WebpageTestCase extends CakeTestCase {
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+        'app.Alias',
+        'plugin.Webpages.Webpage'
+        );
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Webpage = ClassRegistry::init('Webpages.Webpage');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Product);
+
+		parent::tearDown();
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ * @todo this should not work, because validation should fail when there is no name
+ */
+	public function testSave() {
+        
+        $testData = array(
+            'Webpage' => array(
+                'type' => 'content'
+                )
+            );
+        $result = $this->Webpage->add($testData);
+        $this->assertTrue(!empty($this->Webpage->id));
+        
+	}
+
+/**
+ * testAddWithAlias method
+ *
+ * @return void
+ */
+	public function testSaveWithAlias() {
+        
+        $testData = array(
+            'Webpage' => array(
+                'type' => 'content'
+                ),
+            'Alias' => array(
+                'name' => 'lorem-ipsum'
+                )
+            );
+        $this->Webpage->add($testData);
+        $result = $this->Webpage->find('first', array('conditions' => array('Webpage.id' => $this->Webpage->id), 'contain' => array('Alias')));
+        $this->assertEqual($testData['Alias']['name'], $result['Alias']['name']);
+        
+	}
+     
+}
