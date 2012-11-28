f24f5458b23239a6b130c8733fb7570b4bd3cee3
Richard Kersey <github@razorit.com>
2012-11-28 08:54:49 -0800

    nothing

--- app/Config/core.php
+++ app/Config/core.php
@@ -1,4 +1,10 @@
 <?php
+// SECRET PART
+
+Configure::write('Recaptcha.privateKey', '6Lc5xsMSAAAAADJmj-bruuzCYXOeSg5Mf7JTyW3e');
+Configure::write('Recaptcha.publicKey', '6Lc5xsMSAAAAAAoP0DkzEcoBHvHeQ2mO506mHnRY'); // don't know if this is needed
+
+// SECRET END
 ini_set('session.cookie_httponly', true); // used for PCI compliance to make session unavailable to Javascript, can be overwritten on a site by site level
 
 if (defined('SITE_DIR') && file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php')) {
@@ -6,7 +12,26 @@ if (defined('SITE_DIR') && file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php
 	require_once(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php');
 	Configure::write('Session.cookie', 'PHPSESSID');
 
-} else {
+}
+
+// SECRET PART 
+
+else if (
+	!defined('SITE_DIR') && 
+	strpos($_SERVER['HTTP_HOST'], '.zuha.com') &&
+	basename($_SERVER['REQUEST_URI']) == 'site') {
+		define('IS_ZUHA', true);
+		define('SITE_DIR', 'sites'.DS.'zuha.com');
+		require_once(ROOT.DS.'sites'.DS.'zuha.com'.DS.'Config'.DS.'core.php');
+} else if (
+	!defined('SITE_DIR') && 
+	strpos($_SERVER['HTTP_HOST'], '.zuha.com')) {
+		//header('Location: /install/site');
+} 
+
+// END SECRET PART  
+
+else {
 	
 	$debugger = !empty($_GET['debugger']) ? $_GET['debugger'] : 2;
 	// Configure::write('debug', 2);
