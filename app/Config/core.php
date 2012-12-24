036976711a70ff80351de230fbf5193b5b8fd4c3
Richard Kersey <github@razorit.com>
2012-12-23 19:21:57 -0800

    d

--- app/Config/core.php
+++ app/Config/core.php
@@ -1,12 +1,9 @@
 <?php
-
 ini_set('session.cookie_httponly', true); // used for PCI compliance to make session unavailable to Javascript, can be overwritten on a site by site level
 
 if (defined('SITE_DIR') && file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php')) {
-	
 	require_once(ROOT.DS.SITE_DIR.DS.'Config'.DS.'core.php');
 	Configure::write('Session.cookie', 'PHPSESSID');
-	
 } else {
 	echo 'no core.php file exists';
 	die();
