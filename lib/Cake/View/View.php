9c282332be7f05092b5175d5279ba8e33239954e
Richard Kersey <github@razorit.com>
2013-01-02 09:09:20 -0800

    Trying to figure out what is and/or isn't needed with this new directory structure

--- lib/Cake/View/View.php
+++ lib/Cake/View/View.php
@@ -1102,8 +1102,10 @@ class View extends Object {
 				if (strpos($path, DS . 'Plugin' . DS) === false) {
 					if ($plugin) {
 						$themePaths[] = $path . 'Themed' . DS . $this->theme . DS . 'Plugin' . DS . $plugin . DS;
+    					//$themePaths[] = $path . 'Plugin' . DS . $plugin . DS; // zuha, better paths for sites
 					}
 					$themePaths[] = $path . 'Themed' . DS . $this->theme . DS;
+    				//$themePaths[] = $path .  DS; // zuha, better paths for sites
 				}
 			}
 			$paths = array_merge($themePaths, $paths);
