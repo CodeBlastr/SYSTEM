b801958ce80059bc12fee0477a0155c52fc4dfd2
Richard Kersey <github@razorit.com>
2013-01-02 10:59:27 -0800

    Trying to make the Install file work only, when we want it to

--- app/Controller/InstallController.php
+++ app/Controller/InstallController.php
@@ -663,7 +663,7 @@ class InstallController extends Controller {
  * If it is not the first upload then we want access to index() and site() to be restricted.
  */
     protected function _handleSecurity() {
-        if (Configure::read('Install') === false && $userRoleId != 1) {
+        if ((defined('SITE_DIR') || Configure::read('Install') === false) && $userRoleId != 1) {
             $this->message[] = __('Install access restricted.');
             $this->_redirect('/users/users/login');
         }
