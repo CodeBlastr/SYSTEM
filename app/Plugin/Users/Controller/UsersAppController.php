359269dc21ab7e853253c81516b38c7933d2d09c
Richard Kersey <github@razorit.com>
2013-04-18 10:26:45 -0700

    Added the prefix variable to alias.
    You can now have a prefix for aliases built into the element.  This is good for example if you want all blog posts to have a permanent url of blog/something-long.  In that example the blog part can be set so that all blog posts get it by default.
    
    Note it would still be customizable on a post by post basis, and wouldn't even have to have all blog posts under the virtual blog directory.

--- app/Plugin/Users/Controller/UsersAppController.php
+++ app/Plugin/Users/Controller/UsersAppController.php
@@ -1,4 +1,21 @@
 <?php
 class UsersAppController extends AppController {
 	
+	// Idea for overwriting individual functions
+	// The core/app UsersController extends this class which
+	// then does a callback to the class extending it
+	// so that sites --- UsersController can have access to 
+	// over writing a single function instead of the whole class
+	//public function index() {
+	//	$controller = $this->request->controller; 
+	//	App::uses($controller, 'Users.Controller');
+	//	$Controller = new $controller;
+	//	return $Controller->_index();
+	//}
+	
+	// sites/somedomain.com/Locale/Plugin/Users/Controllers/UsersController::index() function example
+	//public function index() {
+	//	here i get to do whatever I want
+	//}
+	
 }
