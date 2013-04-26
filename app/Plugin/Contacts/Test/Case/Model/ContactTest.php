075c1e7f68f347cad7a5451bc604bea880ff4e6d
Richard Kersey <github@razorit.com>
2013-04-25 20:05:09 -0700

    Added a test for a bug that was found where a current contact would have their name set to unknown.

--- app/Plugin/Contacts/Test/Case/Model/ContactTest.php
+++ app/Plugin/Contacts/Test/Case/Model/ContactTest.php
@@ -14,6 +14,8 @@ class ContactTestCase extends CakeTestCase {
 	public $fixtures = array(
 		'plugin.Contacts.Contact',
 		'plugin.Contacts.ContactDetail', // gotta make sure the fixture file exists, not just the config schema!!!
+		'plugin.Contacts.ContactsContact', // gotta make sure the fixture file exists, not just the config schema!!!
+		'plugin.Users.User',
         'plugin.Activities.Activity',
         'plugin.Tasks.Task',
         'plugin.Estimates.Estimate',
@@ -67,6 +69,29 @@ class ContactTestCase extends CakeTestCase {
         $this->assertEqual($result['Contact']['contact_industry'], $data['Contact']['contact_industry']); // test that meta fields work
 	}
 
+/**
+ * test Add Employee
+ * 
+ * @return void
+ */
+	public function testAddEmployee() {
+		$data = array(
+			'Contact' => array(
+				'id' => '4ea754d1-2f50-4f19-8998-4d1245a3a949',
+				'is_company' => '0',
+				'name' => ''
+			),
+			'Employer' => array(
+				'Employer' => '51799607-6440-4cc9-8a72-797145a3a949'
+			)
+		);
+		$result = $this->Contact->add($data);
+		$this->assertEqual($result, 'Contact saved successfully.');
+		
+		$result = $this->Contact->find('first', array('contain' => array('Employer'), 'conditions' => array('Contact.id' => $data['Contact']['id'])));
+		$this->assertEqual($result['Employer'][0]['name'], 'Josie\'s Company');  // shows that the existing Josie was added to Josie's Company
+	} 
+	
 /**
  * test Add With Activity method
  *
@@ -167,7 +192,7 @@ class ContactTestCase extends CakeTestCase {
 		$result = $this->Contact->find('first', array('conditions' => array('Contact.id' => $this->Contact->id), 'contain' => array('Activity', 'Estimate', 'ContactDetail', 'Task')));
 				
         $this->assertEqual($result['Contact']['name'], $data['Contact']['name']); // test that save occured
-        $this->assertEqual($result['Task'][0]['name'], $data['Task'][0]['name']); // test that task is included
+        $this->assertEqual($result['Task'][0]['name'], $data['Task'][0]['name'] . ' ' . $data['Contact']['name']); // test that task is included
         $this->assertEqual($result['Estimate'][0]['total'], $data['Estimate'][0]['total']); // test that estimate is included
         $this->assertEqual($result['Activity'][0]['name'], $data['Contact']['name']); // test that activity is included
         $this->assertEqual($result['Activity'][1]['name'], $data['Activity'][0]['name']); // test that activity is included
