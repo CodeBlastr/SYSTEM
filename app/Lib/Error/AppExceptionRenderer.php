d20dcf33eafb40252d025034f7dc4f0b2c0b6c54
Buildrr LLC <richard@buildrr.com>
2013-06-15 18:22:06 +0000

    Changed some error reporting, so that we get more feedback when there are not working core.php and database.php files.

--- app/Lib/Error/AppExceptionRenderer.php
+++ app/Lib/Error/AppExceptionRenderer.php
@@ -4,24 +4,27 @@ App::uses('ExceptionRenderer', 'Error');
 class AppExceptionRenderer extends ExceptionRenderer {
 		
 	protected function _getController($exception) {
-		App::uses('AppErrorController', 'Controller');
-		if (!$request = Router::getRequest(false)) {
-			$request = new CakeRequest();
-		}
-		$response = new CakeResponse(array('charset' => Configure::read('App.encoding')));
-		$Controller = new AppErrorController($request, $response);
-		
-		try {
-			$Controller->handleAlias($request, $exception); // checks for alias match
-		} catch (Exception $e) {
-			try {
-				$Controller->handleNotFound($request, $response, $e, $exception);
-			} catch (Exception $e) {
-				$Controller = new Controller($request, $response);
-				$Controller->viewPath = 'Errors';
+		if (get_class($exception) == 'MissingControllerException'){
+			App::uses('AppErrorController', 'Controller');
+			if (!$request = Router::getRequest(false)) {
+				$request = new CakeRequest();
 			}
+			$response = new CakeResponse(array('charset' => Configure::read('App.encoding')));
+			$Controller = new AppErrorController($request, $response);
+			try {
+				$Controller->handleAlias($request, $exception); // checks for alias match
+			} catch (Exception $e) {
+				try {
+					$Controller->handleNotFound($request, $response, $e, $exception);
+				} catch (Exception $e) {
+					$Controller = new Controller($request, $response);
+					$Controller->viewPath = 'Errors';
+				}
+			}
+			return $Controller;	
+		} else {
+			return parent::_getController($exception);
 		}
-		return $Controller;
 	} 
 	
 }
