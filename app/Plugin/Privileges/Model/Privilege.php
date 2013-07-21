a74b5fed560e70780bad5b854a6ec415db0c961c
Buildrr LLC <richard@buildrr.com>
2013-07-21 02:29:41 +0000

    Updated Privileges and link_permissions.
    the settings needed for link permissions get written now, and the link_permissions element handles looser matching href values.

--- app/Plugin/Privileges/Model/Privilege.php
+++ app/Plugin/Privileges/Model/Privilege.php
@@ -1,10 +1,10 @@
 <?php
 class Privilege extends PrivilegesAppModel {
 
-	var $name = 'Privilege'; 
-	var $useTable = 'aros_acos';
+	public $name = 'Privilege'; 
+	public $useTable = 'aros_acos';
 	
-	var $belongsTo = array(
+	public $belongsTo = array(
 		'Section' => array(
 	    	'className' => 'Privileges.Section',
 	        'foreignKey' => 'aco_id'
@@ -15,8 +15,18 @@ class Privilege extends PrivilegesAppModel {
 	        )
 		);
 	
+/**
+ * After Save Callback
+ */
+	public function afterSave($created) {
+		$this->_writeLinkPermissions();
+		return parent::afterSave($created);
+	}
 	
-	function prepare(){
+/**
+ * Prepare method
+ */
+	public function prepare(){
 		$dat = $this->find('all', array(
 			'contain' => array()
 			));
@@ -66,29 +76,62 @@ class Privilege extends PrivilegesAppModel {
 		return $ret;
 	}
 	
-	/*
-	 * Checks if record exists 
-	 * @param {int} aro_id
-	 * @param {int} aco_id
-	 * @return {mixed}
-	 */
-
-	function checkSection($requestor_id , $aco_id){
+/**
+ * Checks if record exists 
+ * @param {int} aro_id
+ * @param {int} aco_id
+ * @return {mixed}
+ */
+	public function checkSection($requestor_id , $aco_id){
 		$cnt = $this->find('first' , array(
-					'conditions'=>array(
-						'Privilege.aro_id' => $requestor_id,
-						'Privilege.aco_id' => $aco_id
-					),
-					'contain'=>array(
-					)
-		));
+			'conditions'=>array(
+				'Privilege.aro_id' => $requestor_id,
+				'Privilege.aco_id' => $aco_id
+				),
+			'contain'=>array(
+				)
+			));
 		
 		if(!isset($cnt["Privilege"]["id"])){
 			return false;
-		}else{
+		} else {
 			return $cnt["Privilege"]["id"];
 		}
+	}
+	
+/**
+ * Write link permissions method
+ * 
+ */
+	protected function _writeLinkPermissions() {
+		$acos = array();
+		$privileges = $this->find('all', array('conditions' => array('Privilege._create' => 1, 'Privilege._read' => 1, 'Privilege._update' => 1, 'Privilege._delete' => 1)));
+		foreach ($privileges as $privilege) {
+			if (!empty($acos[$privilege['Privilege']['aco_id']])) {
+				$acos[$privilege['Privilege']['aco_id']] = $acos[$privilege['Privilege']['aco_id']] . ',' . $privilege['Privilege']['aro_id'];
+			} else {
+				$acos[$privilege['Privilege']['aco_id']] = $privilege['Privilege']['aro_id'];
+			}
+		}
 		
+		$settings = '';
+		foreach ($acos as $aco => $aros) {
+			$path = $this->Section->getPath($aco); // all of the acos parents
+			if ($path === null) {
+				// if path is null we need to delete the aros_acos that use that aco because it doesn't exist
+				$this->deleteAll(array('Privilege.aco_id' => $aco));
+			} else {
+				$url = str_replace('controllers', '', Inflector::singularize(Inflector::tableize(ZuhaInflector::flatten(Set::extract('/Section/alias', $path), array('separator' => '/')))));
+				$settings .= $url . ' = ' . $aros . PHP_EOL;
+			}
+		}		
+		App::uses('Setting', 'Model');
+		$Setting = new Setting;
+		
+		$data['Setting']['type'] = 'APP';
+ 		$data['Setting']['name'] = 'LINK_PERMISSIONS';
+ 		$data['Setting']['value'] = trim($settings);
+		$Setting->add($data);
 	}
 	
 }
