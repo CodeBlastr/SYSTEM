c0632fca00f39cdb1aa4cf9123eaa12e8ad58d77
Buildrr LLC <richard@buildrr.com>
2013-07-29 20:51:36 +0000

    added user to user unit tests

--- app/Plugin/Users/Test/Fixture/UserFixture.php
+++ app/Plugin/Users/Test/Fixture/UserFixture.php
@@ -42,5 +42,29 @@ class UserFixture extends CakeTestFixture {
 			'created' => '1642-12-25 09:44:22',
 			'modified' => '1727-03-20 05:32:44'
 		),
+		array(
+			'id' => 101,
+			'parent_id' => null,
+			'reference_code' => null,
+			'full_name' => 'Dolly Parton',
+			'first_name' => 'Dolly',
+			'last_name' => 'Parton',
+			'username' => 'dolly.parton',
+			'password' => 'e85d8e4e8f1c7e4c91016c200f9c57e13bc8d3a9',
+			'email' => 'dolly.parton@razorit.com',
+			'view_prefix' => null,
+			'zip' => null,
+			'last_login' => '1927-03-20 05:32:44',
+			'forgot_key' => null,
+			'forgot_key_created' => null,
+			'forgot_tries' => null,
+			'user_role_id' => 3,
+			'credit_total' => null,
+			'slug' => null,
+			'creator_id' => null,
+			'modifier_id' => null,
+			'created' => '1842-12-25 09:44:22',
+			'modified' => '1927-03-20 05:32:44'
+		),
 	);
 }
