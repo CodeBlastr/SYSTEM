9991f44ae9bc8efaa086edc44c7d7f090f237ecc
Nick <nick@buildrr.com>
2013-09-10 15:38:15 -0400

    Removed Unique on name. Need to be able to have different menus with the same link text

--- app/Plugin/Webpages/Config/Schema/schema.php
+++ app/Plugin/Webpages/Config/Schema/schema.php
@@ -52,8 +52,7 @@ class WebpagesSchema extends CakeSchema {
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1), 
-			'name' => array('column' => 'name', 'unique' => 1)
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
