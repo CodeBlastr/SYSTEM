b5374790c57775b1c59ad9870a6bc38e3f241ab1
Buildrr LLC <richard@buildrr.com>
2013-12-13 17:26:56 +0000

    formatting

--- app/Model/Behavior/MetableBehavior.php
+++ app/Model/Behavior/MetableBehavior.php
@@ -26,9 +26,9 @@ class MetableBehavior extends ModelBehavior {
  * @param Model $Model
  * @param type $settings
  */
-	public function setup(Model $Model, $settings = array()) {
-        return true;
-	}
+	// public function setup(Model $Model, $settings = array()) {
+        // return true;
+	// }
     
 /**
  * After save callback
@@ -39,16 +39,16 @@ class MetableBehavior extends ModelBehavior {
  * @param boolean $created The value of $created will be true if a new record was created (rather than an update).
  */
 	public function afterSave(Model $Model, $created, $options = array()) {
-        
+        debug($Model->data);
 		if ( !empty($Model->data[$Model->alias]['Meta']) && is_array($Model->data[$Model->alias]['Meta']) ) {
 			$metadata = $Model->data[$Model->alias]['Meta'];
-			unset( $Model->data[$Model->alias]['Meta'] );
+			unset($Model->data[$Model->alias]['Meta']);
 		}
 
-		if ( !empty($metadata) ) {
+		if (!empty($metadata)) {
             $Meta = ClassRegistry::init('Meta');
 			$existingMeta = $Meta->find( 'first', array('conditions' => array('model' => $Model->name, 'foreign_key' => $Model->id)) );
-			if ( !$existingMeta ) {
+			if (!$existingMeta) {
 				$cleanMetadata = mysql_escape_string( serialize($metadata) ); 
 				$Meta->query("
 					INSERT INTO `metas` (model, foreign_key, value)
@@ -57,17 +57,17 @@ class MetableBehavior extends ModelBehavior {
 			} else {
 				// Meta already exists, update it. The incoming data, $metadata, needs to overwrite current values.
 				// extract array from $existingMeta['Meta']['value']
-				$existingMetaValue = unserialize( $existingMeta['Meta']['value'] );
+				$existingMetaValue = unserialize($existingMeta['Meta']['value']);
 
-				foreach ( $existingMetaValue as $k => $v ) {
+				foreach ($existingMetaValue as $k => $v) {
 					// clean out obsolete exclamation points
-					if ( strstr($k, '!') ) {
+					if (strstr($k, '!')) {
 						$noPoint = str_replace('!', '', $k);
 						$existingMetaValue[$noPoint] = $v;
-						unset( $existingMetaValue[$k] );
+						unset($existingMetaValue[$k]);
 					}
 				}
-
+					
 				// merge that array with $metadata
 				$updatedMetaValue = ZuhaSet::array_replace_r( $existingMetaValue, $metadata );
 
@@ -80,6 +80,7 @@ class MetableBehavior extends ModelBehavior {
 				");
 			}
 		}
+
 		parent::afterSave($Model, $created, $options);
 	}
     
@@ -181,18 +182,17 @@ class MetableBehavior extends ModelBehavior {
  * @return array
  */
     public function mergeSerializedMeta(Model $Model, $results = array()) {
-		foreach($results as &$result) {
-//			debug($result);
-			if(isset($result['Meta']['foreign_key'])) {
+		foreach ($results as &$result) {
+			if (isset($result['Meta']['foreign_key'])) {
 				// merges the unserialized Meta values into the Model.Meta array
 				$result[$Model->alias]['Meta'] = unserialize($result['Meta']['value']);
 				
 				// clean out obsolete exclamation points
-				foreach ( $result[$Model->alias]['Meta'] as $k => $v ) {
-					if( strstr($k, '!') ) {
+				foreach ($result[$Model->alias]['Meta'] as $k => $v) {
+					if (strstr($k, '!')) {
 						$noPoint = str_replace('!', '', $k);
 						$result[$Model->alias]['Meta'][$noPoint] = $v;
-						unset( $result[$Model->alias]['Meta'][$k] );
+						unset($result[$Model->alias]['Meta'][$k]);
 					}
 				}
 				
@@ -282,7 +282,7 @@ class MetableBehavior extends ModelBehavior {
  * and if we need them, they might be better in the hasOne['fields'] area
  */
 	protected function _queryFields(Model $Model, $query) {
-		if(!empty($query['fields']) && is_array($query['fields'])) {
+		if (!empty($query['fields']) && is_array($query['fields'])) {
 			$query['fields'][] = 'Meta.model';
 			$query['fields'][] = 'Meta.foreign_key';
 			$query['fields'][] = 'Meta.value';
@@ -301,14 +301,13 @@ class MetableBehavior extends ModelBehavior {
  * @return array
  */
 	protected function _queryConditions(Model $Model, $query) {
-        
-		if(!empty($query['conditions']) && is_array($query['conditions'])) {
+		if (!empty($query['conditions']) && is_array($query['conditions'])) {
 			foreach($query['conditions'] as $condition => $value) {
-				if(strstr($condition, $Model->alias.'.Meta.')) {
+				if (strstr($condition, $Model->alias.'.Meta.')) {
 					$Model->metaConditions[str_replace($Model->alias.'.', '', $condition)] = $value;
 					unset($query['conditions'][$condition]);
 				}
-				elseif(strstr($condition, $Model->alias.'.!')) {  // support deprecated !fields
+				elseif (strstr($condition, $Model->alias.'.!')) {  // support deprecated !fields
 					$Model->metaConditions[str_replace($Model->alias.'.!', '', $condition)] = $value;
 					unset($query['conditions'][$condition]);
 				}
@@ -335,6 +334,5 @@ class MetableBehavior extends ModelBehavior {
 		}
 		return $results;
 	}
-
 
 }
