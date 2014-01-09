3fd6af20e982c3478489b3a24dc4b1e8caedc17e
Joel Byrnes <joel@razorit.com>
2014-01-09 12:27:03 -0500

    remove redundant Comments binding; add call to parent::__construct()

--- app/Plugin/Users/Model/UserGroupWallPost.php
+++ app/Plugin/Users/Model/UserGroupWallPost.php
@@ -2,9 +2,8 @@
 App::uses('UsersAppModel', 'Users.Model');
 
 class UserGroupWallPost extends UsersAppModel {
-	public $name = 'UserGroupWallPost';
-
 	
+	public $name = 'UserGroupWallPost';
 	
 	public $belongsTo = array(
 		'UserGroup' => array(
@@ -24,14 +23,9 @@ class UserGroupWallPost extends UsersAppModel {
 	);
 	
 	public function __construct($id = false, $table = null, $ds = null) {
-		if(CakePlugin::loaded('Comments')) {
+		if (CakePlugin::loaded('Comments')) {
 			$this->actsAs[] = 'Comments.Commentable';
-			$this->hasMany[] = array(
-					'Comment' => array(
-						'className' => 'Comments.Comment',
-						'foreignKey' => 'foreign_key',
-						'conditions' => array('Comment.model' => 'UserGroupWallPost')
-					));
 		}
+		parent::__construct($id, $table, $ds);
 	}
 }
