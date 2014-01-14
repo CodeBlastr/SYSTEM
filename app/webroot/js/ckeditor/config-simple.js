9d2dd43949c4d5fcc7bc8ea1edbe6e42ccfba8bc
Yingjun Cui <yingjun@buildrr.com>
2014-01-14 17:31:45 -0500

    upgrade ckeditor to v4.3

---
+++ app/webroot/js/ckeditor/config-simple.js
@@ -0,0 +1,145 @@
+$(function(){
+    window.simpletext = window.simpletext || {};
+    /**
+     * @param $ | jQuery
+     * @param CK | CKEDITOR
+     * @param w | window
+     *
+     */
+    (function($,CK,w){
+        var MyEditor = function($container){
+            if($container.length){
+                var self = this,
+                    $buttons = $container.find('button');
+
+                if(!$buttons.length){
+                    $buttons = $container.find('a');
+                }
+                if($buttons.length){
+                    $buttons.each(function(){
+                        var action = 'self.'+$(this).data('action');
+                        if($.isFunction(eval(action))){
+                            $(this).click(function(){
+                                eval(action+'()');
+                            });
+                        }
+
+                    });
+                }
+            }
+
+        };//editor class
+        MyEditor.prototype = (function(){
+
+            var configEditor = function(toolbarButtons,callback){
+                var instanceId = editor_id;
+                var editor = CK.instances[instanceId],
+                    defaultButtons = ['Bold','Italic','Underline',
+                        '-','JustifyLeft','JustifyCenter','JustifyRight','-','NumberedList','BulletedList','Blockquote'];
+
+                if(editor){	editor.destroy(true);}
+
+                toolbarButtons = $.isArray(toolbarButtons) ? defaultButtons.concat(toolbarButtons) : defaultButtons;
+
+                CK.replace(instanceId,{
+                    toolbar:[toolbarButtons],
+                    on : {
+                        instanceReady : function(event){
+                            if($.isFunction(callback)){
+                                callback(event.editor);
+                            }
+                        }
+                    }
+                });
+
+
+
+            };
+            /**
+             *
+             * @param remoteContentsList | array of tab ids which you want to remove from dialog
+             * @param func | callback function
+             */
+            var customDialog = function(remoteContentsList,func){
+                CKEDITOR.on( 'dialogDefinition', function( ev ) {
+                    // Take the dialog name and its definition from the event data.
+                    var dialogName = ev.data.name,
+                        dialogDefinition = ev.data.definition;
+
+
+                    for(var index in remoteContentsList){
+                        dialogDefinition.removeContents(remoteContentsList[index]);
+                    }
+                    func(dialogName,dialogDefinition);
+                });
+            }
+            return{
+                'text':function(){
+
+                },//text method end
+                'links':function(){
+
+                    configEditor(['sharelink'],function(editor){
+                        editor.commands.sharelink.exec();
+                    });
+                },//links methods end
+                'photos':function(){
+
+                    configEditor(['Image'],function(editor){
+
+
+                        customDialog(['info','Link','advanced'],function(dialogName,dialogDefinition){
+
+                            if ( dialogName == 'image' ) {
+                                var uploadButton = dialogDefinition.getContents( 'Upload').get('uploadButton');
+                                dialogDefinition.onShow = function(){};//overwrite onShow function
+                                if(uploadButton){
+                                    uploadButton['filebrowser']['onSelect'] = function(fileUrl,errorMessage){
+                                        if(!errorMessage){
+                                            editor.insertHtml('<img src="'+fileUrl+'" />');
+                                            CKEDITOR.dialog.getCurrent().hide();
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                        editor.commands.image.exec();
+                    });
+                },//photos method end
+
+                'audios':function(){
+                    this.videos();
+                },//audios method end
+
+                'videos':function(){
+
+                    configEditor(['oembed'],function(editor){
+                        editor.commands.oembed.exec();
+                    });
+                },//videos method end
+                'documents':function(){
+
+                    configEditor(['Link'],function(editor){
+                        editor.commands.link.exec();
+                        customDialog(['info','target','advanced'],function(dialogName,dialogDefinition){
+                            if ( dialogName == 'link' ) {
+
+                                var uploadButton = dialogDefinition.getContents('upload').get('uploadButton');
+
+                                if(uploadButton){
+                                    uploadButton['filebrowser']['onSelect'] = function(fileUrl,errorMessage){
+                                        if(!errorMessage){
+                                            editor.insertHtml('<p><a href="'+fileUrl+'">'+fileUrl+'</a></p>');
+                                            CKEDITOR.dialog.getCurrent().hide();
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    });
+                }//documents method end
+            };
+        })();
+        w.simpletext.ck = new MyEditor(jQuery('#editor-buttons'));
+    })(jQuery,CKEDITOR,window);
+})
---
+++ app/webroot/js/ckeditor/plugins/sharelink/plugin.js
@@ -0,0 +1,26 @@
+﻿/**
+*
+*/
+
+(function(){
+
+    CKEDITOR.plugins.add( 'sharelink', {
+        icons: 'sharelink',
+        init: function( editor ) {
+            editor.addCommand( 'sharelink', new CKEDITOR.dialogCommand( 'sharelinkDialog' ));
+
+            editor.ui.addButton('sharelink', {
+                label: 'sharelink',
+                command: 'sharelink',
+                toolbar: 'insert',
+                icon: this.path + "images/" + (CKEDITOR.env.hidpi ? "hidpi/" : "") + "anchor.png"
+            });
+
+            CKEDITOR.dialog.add( 'sharelinkDialog', this.path + 'dialogs/dialog.js' );
+        }
+
+
+    });
+
+
+})();
