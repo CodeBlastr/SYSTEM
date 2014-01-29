5215c5192c2c8f53aef39763f3430b52ace1eba6
Yingjun Cui <yingjun@buildrr.com>
2014-01-29 12:18:23 -0500

    youforthem invations function

--- app/Plugin/Users/Model/UserFollower.php
+++ app/Plugin/Users/Model/UserFollower.php
@@ -22,6 +22,11 @@ class UserFollower extends UsersAppModel {
         )
 	);
 
+	public function beforeSave($options = array()) {
+		return parent::beforeSave($options);
+	}
+
+
 	private function _changeStatus($requestId,$userId,$status){
 		$row = $this->read(array('approved','user_id'),$requestId);
 		if(!empty($row) && $row['UserFollower']['user_id'] == $userId){
@@ -31,6 +36,7 @@ class UserFollower extends UsersAppModel {
 					'id'	=>$requestId
 				),
 			);
+
 			return $this->save($data);
 		}
 		return false;
@@ -40,7 +46,7 @@ class UserFollower extends UsersAppModel {
 	}
 
 	public function decline($requestId,$userId){
-		$this->_changeStatus($requestId,$userId,-2);
+		$this->_changeStatus($requestId,$userId,-1);
 	}
 
 
@@ -49,7 +55,7 @@ class UserFollower extends UsersAppModel {
  * @param $followerId | user id
  * @return int $returnValue  | 1 -- friends(approved)
  *							 | 0 -- request sent, waiting for approve
- *                           | -1 -- not follow(no request sent, not followed, no relationship)
+ *                           | -1 -- not follow(no request sent, not followed, no relationship, or be declined)
  * 							 | >1 -- waiting approve by me, return id in user_followers table
  */
 	public function checkFollowStatus($uid, $followerId){
@@ -61,7 +67,7 @@ class UserFollower extends UsersAppModel {
 			),
 		));
 		if(!empty($isSentMeRequest)){
-			$returnValue = $isSentMeRequest['UserFollower']['approved'] === true ? true :$isSentMeRequest['UserFollower']['id'];
+			$returnValue = $isSentMeRequest['UserFollower']['approved'] == 1 ? true :$isSentMeRequest['UserFollower']['id'];
 		} else {
 			$isSentRequest = $this->find('first',array(
 				'conditions'=>array(
@@ -70,13 +76,16 @@ class UserFollower extends UsersAppModel {
 				)
 			));
 			if(!empty($isSentRequest)){
-				$returnValue = $isSentRequest['UserFollower']['approved'] === true ? true : 0;
+				$returnValue = $isSentRequest['UserFollower']['approved'] == 1 ? true : 0;
 			}
 		}
 		return $returnValue;
 	}
+	public function isMyRequest($requestId,$userId){
+		return $this->find('count',array('conditions'=>array('id'=>$requestId,'user_id'=>$userId))) == 1;
 
-	public	function getPendingRequetCount($userId){
+	}
+	public function getPendingRequetCount($userId){
 		return $this->find('count',array('conditions'=>array(
 			'approved' => null,
 			'user_id'	=>$userId,
