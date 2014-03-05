6cba56293ee0dc0563bd9380da0ade2a2888d912
Nick <nick@buildrr.com>
2014-03-05 17:00:17 -0500

    added random password generator

--- app/Plugin/Users/Model/User.php
+++ app/Plugin/Users/Model/User.php
@@ -1070,6 +1070,21 @@ Thank you for registering with us and welcome to the community.";
 		 $Rating = new Rating; //create Object $Rating
 		 return $Rating->save($data); //return data and save
 	}
+	
+	/**
+	 * Generate a Random Password
+	 * @return string
+	 */
+	function randomPassword() {
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < 8; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		return implode($pass); //turn the array into a string
+	}
 
 }
 
