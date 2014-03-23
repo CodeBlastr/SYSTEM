83a56ee4dbad0f4ad715fdde80fa9d60a8cefd83
Arindrajit Developer <arindrajit@buildrr.com>
2014-03-23 08:15:44 -0700

    Added required file for a plugin

---
+++ app/Config/generic_auth.php
@@ -0,0 +1,17 @@
+<?php
+if (file_exists(ROOT.DS.SITE_DIR.DS.'Config'.DS.'generic_auth.php')) :
+	require_once(ROOT.DS.SITE_DIR.DS.'Config'.DS.'generic_auth.php');
+else :
+	/**
+	  * Get an api_key and secret from facebook and fill in this content.
+	  * save the file to app/config/facebook.php
+	  */
+	// $config = array(
+	  // 'Facebook' => array(
+	  	// 'appId' => '315141015179110',
+	    // 'apiKey' => '315141015179110',
+	    // 'secret' => 'd478fd08b4096607dc68d8b803fc9ef5',
+	    // 'cookie' => true,
+	  // )
+	// );
+endif;
