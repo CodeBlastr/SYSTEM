f5732bef8715570004c19dd7c26632031b17a2ac
Buildrr LLC <richard@buildrr.com>
2015-09-12 15:58:57 +0000

    added support for site overwrites

--- app/Plugin/Users/Model/UserGroupWallPost.php
+++ app/Plugin/Users/Model/UserGroupWallPost.php
@@ -1,7 +1,7 @@
 <?php
 App::uses('UsersAppModel', 'Users.Model');
 
-class UserGroupWallPost extends UsersAppModel {
+class AppUserGroupWallPost extends UsersAppModel {
 	
 	public $name = 'UserGroupWallPost';
 	
@@ -40,4 +40,8 @@ class UserGroupWallPost extends UsersAppModel {
 		}
 		parent::__construct($id, $table, $ds);
 	}
+}
+
+if (!isset($refuseInit)) {
+	class UserGroupWallPost extends AppUserGroupWallPost {}
 }
